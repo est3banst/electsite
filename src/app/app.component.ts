e2276bb0b634ec910182f6dece142ef27f25da51
est3banst <estebeeen@outlook.com>
2024-01-10 19:04:26 -0300

    fixed more sizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-import { NgModule } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+
+  isMobile = true;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+  ) {
+
+    this.breakpointObserver.observe([
+      "(max-width: 460px)"
+    ]).subscribe((result: BreakpointState) => {
+      if (result.matches) {
+         this.isMobile = true;
+         console.log(this.isMobile);
+      } else {
+          this.isMobile = false;
+          console.log(this.isMobile);
+      }
+    });
+  }
 
 
-  title = 'hugojuresa';
 }
+
+
+
+
+
